0a21aab34e4c66582c2d22b0113a25647f04d357
batmansachin <sachin.pitbull.12@gmail.com>
2018-05-01 10:38:00 +0530

    Update Utility.java

--- FriendsRecords/src/FriendList/Utility.java
+++ FriendsRecords/src/FriendList/Utility.java
@@ -1,34 +1,26 @@
-
 package FriendList;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-
 import org.json.simple.JSONArray;
-import org.json.JSONException;
-import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
 public class Utility {
-	
-public static JSONArray readJson(String filename) {
+
+    public static JSONArray readJson(String filename) {
         JSONParser parser = new JSONParser();
         JSONArray jsonArr = null;
-        try{
+        try {
             jsonArr = (JSONArray) parser.parse(new FileReader(filename));
-	}
-	catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (org.json.simple.parser.ParseException e) {
             e.printStackTrace();
         }
-		return jsonArr;
-	}
-	
-	
-}
+        return jsonArr;
+    }
 
+}
