03f7275e3cdf5bc5fa67f8e6f8091faf81b26303
batmansachin <sachin.pitbull.12@gmail.com>
2018-05-01 10:37:29 +0530

    Update Main.java

--- FriendsRecords/src/FriendList/Main.java
+++ FriendsRecords/src/FriendList/Main.java
@@ -1,19 +1,15 @@
-
 package FriendList;
 
-import org.json.JSONArray;
 import org.json.JSONException;
-//import org.json.simple.JSONException;
 
 public class Main {
 
-	public static void main(String[] args) throws JSONException {
-		
-           
-            org.json.simple.JSONArray jsonArray = Utility.readJson(FriendConstants.friendsFilePath);
-            FriendManager friends = new FriendManager(jsonArray); 
-            friends.sort();
-            friends.printStudents(FriendConstants.baseLat, FriendConstants.baseLon, FriendConstants.thresholdDistance);
-	}
-	
+    public static void main(String[] args) throws JSONException {
+
+        org.json.simple.JSONArray jsonArray = Utility.readJson(FriendConstants.friendsFilePath);
+        FriendManager friends = new FriendManager(jsonArray);
+        friends.sort();
+        friends.printStudents(FriendConstants.baseLat, FriendConstants.baseLon, FriendConstants.thresholdDistance);
+    }
+
 }
