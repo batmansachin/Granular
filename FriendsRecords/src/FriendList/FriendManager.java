a885ee8b98f266ba308a5ea8dd47991ba4236250
batmansachin <sachin.pitbull.12@gmail.com>
2018-05-01 10:36:57 +0530

    Update FriendManager.java

--- FriendsRecords/src/FriendList/FriendManager.java
+++ FriendsRecords/src/FriendList/FriendManager.java
@@ -1,5 +1,5 @@
-
 package FriendList;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import org.json.JSONException;
@@ -8,37 +8,38 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class FriendManager {
-	
-	private ArrayList<Friend> friends;
-	
-	public FriendManager(JSONArray friendsArr) throws JSONException{
-		friends = new ArrayList<Friend>();
-		for (Object frndObj : friendsArr) {
-			Friend friend = new Friend((org.json.simple.JSONObject) frndObj);
-			friends.add(friend);    
-		  }
-	}		
-	private ArrayList<Friend> keepNearby(double baseLat, double baseLon, int thresholdDistance){
-		ArrayList<Friend> nearbyFriends = new ArrayList<Friend>();
-		for(Friend frnd: friends){
-			double frndDistance = frnd.distance(baseLat, baseLon);
-			if(frndDistance <= thresholdDistance){
-				nearbyFriends.add(frnd);
-			}
-		}
-		return nearbyFriends;
-	}
-	
-	public void sort() {
-		Collections.sort(friends);
-	}
 
-	public void printStudents(double baseLat, double baseLon, int thresholdDistance){
-                
-                ArrayList<Friend> NearFriends = keepNearby(baseLat, baseLon, thresholdDistance);
-		for(Friend frnd: NearFriends){
-                    
-			System.out.println(frnd);
-		}
-	}
+    private ArrayList<Friend> friends;
+
+    public FriendManager(JSONArray friendsArr) throws JSONException {
+        friends = new ArrayList<Friend>();
+        for (Object frndObj : friendsArr) {
+            Friend friend = new Friend((org.json.simple.JSONObject) frndObj);
+            friends.add(friend);
+        }
+    }
+
+    private ArrayList<Friend> keepNearby(double baseLat, double baseLon, int thresholdDistance) {
+        ArrayList<Friend> nearbyFriends = new ArrayList<Friend>();
+        for (Friend frnd : friends) {
+            double frndDistance = frnd.distance(baseLat, baseLon);
+            if (frndDistance <= thresholdDistance) {
+                nearbyFriends.add(frnd);
+            }
+        }
+        return nearbyFriends;
+    }
+
+    public void sort() {
+        Collections.sort(friends);
+    }
+
+    public void printStudents(double baseLat, double baseLon, int thresholdDistance) {
+
+        ArrayList<Friend> NearFriends = keepNearby(baseLat, baseLon, thresholdDistance);
+        for (Friend frnd : NearFriends) {
+
+            System.out.println(frnd);
+        }
+    }
 }
